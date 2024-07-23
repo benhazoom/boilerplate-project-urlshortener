06e87468c58fe75bfb00f0058aa4e37f697a4a1c
benhazoom <87472603+benhazoom@users.noreply.github.com>
2024-07-23 15:00:20 +0300

    successful fetching data from db

--- index.js
+++ index.js
@@ -2,16 +2,26 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const asyncHandler = require ('express-async-handler');
 var bodyParser = require('body-parser')
+const dns = require('dns');
+const mongoose = require('mongoose')
+console.log(process.env.dbURI);
+const Url = require('./models/url');
+let index = 1;
 
-var inMemoryUrlDataBase = []; // In-memory data store for simplicity
-let index = 0;
-// const dns = require('dns');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+
+//, { useNewUrlParser: true, useUnifiedTopology: true }
+//MongoDB
+const client = mongoose.connect(process.env.dbURI)
+.then(result => app.listen(3000))
+.catch(err => console.log(err));
+
 //middleweres from bodyParser
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -23,23 +33,26 @@ app.get('/', function(req, res) {
 
 // Your first API endpoint
 app.post('/api/shorturl', function(req, res) {
-  const url = (req.body.url);
-  //invalid url
-  if (!url.startsWith("https://")) res.json({ error: 'invalid url' });
-  //saving shortened url locally
-  inMemoryUrlDataBase.push(url);
-  index++;
-  // let formerIndex=index-1;
-  res.json({
-    original_url: url, 
-    short_url:index-1
+  console.log(req.body);
+  const newUrl = Url ({url:req.body.url,short_url:index});
+  newUrl.save()
+  .then((result)=>{
+    index++;
+    res.json({original_url:newUrl.url,url:newUrl.short_url});
+  })
+  .catch((err)=>{
+  console.log(err);
   });
 });
 
-app.get('/api/shorturl/:id', function(req, res) {
-  console.log(inMemoryUrlDataBase[req.params.id])
-  res.redirect(inMemoryUrlDataBase[req.params.id]);
-});
+app.get('/api/shorturl/:id', asyncHandler(async (req, res) => {
+  //
+  console.log("hey")
+  const url = await Url.findOne({short_url:req.params.id});
+  const originalUrl = url.url;
+  res.redirect(originalUrl) ;
+}));
+  
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
